91d338a69ba0e62b4605cf62ebad747df5eb9ea0
Jakir Khan <jakirkhan71@gmail.com>
2022-12-08 09:36:40 +0600

    initial commit

---
+++ src/main/java/com/avatar/user/AvaterApplication.java
@@ -0,0 +1,13 @@
+package com.avatar.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaterApplication.class, args);
+	}
+
+}
